73c13cce8679f3546d6ebe91fbf0c402256939c6
ARKK <arkkgraphics@gmail.com>
2025-10-04 09:27:28 +0800

    Initial commit: Death Note parody site with Astro + Tailwind

---
+++ README.md
@@ -0,0 +1,10 @@
+# Death Note Parody Site
+
+A parody one-pager for cosplay/alt culture community.
+
+## Setup
+npm install
+npm run dev
+
+## Deploy to Vercel
+Push to GitHub, connect repo to Vercel.
---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'death-black': '#0a0a0a',
+        'blood-red': '#8B0000',
+        'bone-white': '#f5f5f0'
+      }
+    }
+  }
+}
